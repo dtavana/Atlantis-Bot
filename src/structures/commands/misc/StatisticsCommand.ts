d1dfe2a4610d0901bbc18b64ba459dfdf104e27b
Darian Tavana <datavana@gmail.com>
2020-01-01 10:58:20 -0500

    Stats command done, currently used for testing

---
+++ src/structures/commands/misc/StatisticsCommand.ts
@@ -0,0 +1,16 @@
+import {Command} from 'discord-akairo';
+import {Message} from 'discord.js';
+import {COMMAND_CATEGORIES} from '../../../lib/constants';
+
+export default class StatisticsCommand extends Command {
+    public constructor() {
+        super('statistics', {
+            aliases: ['statistics', 'stats'],
+            category: COMMAND_CATEGORIES.MISC,
+        });
+    }
+
+    public async exec(message: Message) {
+        this.client.sendLog('Stats command run');
+    }
+}
